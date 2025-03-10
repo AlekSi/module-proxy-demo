4e43d42f45252848fe4b665fc173cf8e82336527
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2025-03-10 09:43:39 +0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AlekSi/module-proxy-demo
+
+go 1.24
+
+toolchain go1.24.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/AlekSi/module-proxy-demo/demo"
+
+func main() {
+	demo.Demo()
+}
